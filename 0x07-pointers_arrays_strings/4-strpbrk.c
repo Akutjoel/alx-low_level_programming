24f59a41f9e81b5c3b264a6a0357516350a95d5b
akutjoel <joelakut1@gmail.com>
2023-03-07 16:24:30 +0000

    Write a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,12 +16,13 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++) /* iterate through target */
 		{
-			if (s[i] == accept[j]) /* stop at first match */
+			if (s[i] == accept[j]) /* stop at first match*?	*/
+			{
 				s = &s[i]; /* set pointer to first occurence */
 			return (s);
+			}
 		}
+		i++;
 	}
-	i++;
-}
-return (NULL); /* return NULL if no matches */
+	return (NULL); /* return NULL if no matches */
 }
